1baad17cac3fc909842ecc5b332b687a22e60b5b
Bercea Cristian <cristiberc@gmail.com>
2025-03-06 14:49:00 +0200

    Added lab2 module's tests and a generated jar

--- lab2/src/test/java/ComputerRepairRequestTest.java
+++ lab2/src/test/java/ComputerRepairRequestTest.java
@@ -6,7 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ComputerRepairRequestTest {
-    
+    @Test
+    @DisplayName("First Test")
+    public void firstTest() {
+        ComputerRepairRequest computerRepairRequest = new ComputerRepairRequest();
+        assertEquals("",computerRepairRequest.getOwnerName());
+        assertEquals("",computerRepairRequest.getOwnerAddress());
+        assertEquals("",computerRepairRequest.getModel());
+    }
 
-      
+    @Test
+    @DisplayName("Test Example")
+    public void exampleTest() {
+        assertEquals(2,2,"Numerele trebuie sa fie egale");
+    }
 }
